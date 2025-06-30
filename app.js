fd9209b4c95c801027649149d44d52bf256b1a06
RobertoMatheus <49045127+RobertoMatheus@users.noreply.github.com>
2025-06-30 10:15:28 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
-const {Produto} = require("./Produto");
+const {Produto} = require("./Produto");   
+const prompt = require("prompt-sync")(); 
 async function testarInsercao() {
-    const user = new Produto("Martelo",
-15.73, 3476,"Ferramenta");
+    let nome = prompt("Digite o nome do produto: ");
+    let preço = prompt("Digite o preço do produto: ");
+    let codigo = prompt("Digite o codigo do produto: ");
+    let tipo = prompt("Digite o tipo do produto: ");
+    const user = new Produto(nome,
+preço, codigo,tipo);
  
  await user.inserir();
- //Substituir console.log por logger, registrar no arquivo de texto
+} 
+async function testarBusca() {
+    const user = new Produto("Chave",
+15.73, 3476,"Ferramenta");
+ 
+ await user.buscar();
+} 
+async function testarDelete() {
+    const user = new Produto("Chave",
+15.73, 3476,"Ferramenta");
+ 
+ await user.deletar();
 }
+console.log("Digite 1 para inserir um produto:");
+console.log("Digite 2 para buscar um produto:");
+console.log("Digite 3 para deletar um produto:");
+  let person = prompt(" ");
+
+    switch(person){
+        case "1":
+            testarInsercao();
+            break;
+        case "2":
+            testarBusca();
+            break;
+        case "3":
+            testarDelete();
+            break;
+    }
 
 /*
  const user = new Produto("Maça",
@@ -15,4 +47,3 @@ async function testarInsercao() {
 */
 
 
-testarInsercao();
